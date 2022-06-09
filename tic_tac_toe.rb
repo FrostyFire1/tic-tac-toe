956164eb52e5ab63a9bd3f6085a043c33bef41f1
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-09 10:41:03 +0200

    Improve print_board logic

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -14,10 +14,8 @@ end
 class Game
   attr_accessor(:player1, :player2)
 
-  def initialize (player1, player2)
-    if player1.symbol == player2.symbol
-      raise ArgumentError, "Players can't have the same symbol!"
-    end
+  def initialize(player1, player2)
+    raise ArgumentError, "Players can't have the same symbol!" if player1.symbol == player2.symbol
 
     @board = Array.new(3) { Array.new(3) }
     @player1 = player1
@@ -29,24 +27,39 @@ class Game
   def start
     play_turn(player1)
   end
-  
+
   def debug
     print_board
   end
 
   private
 
+  def win?
+    false
+  end
+
   def print_board
-    0.upto(@board.length-2) do |row|
-     0.upto(@board[row].length-2) do |i|
-      print "#{@board[row][i]} | "
-     end
-     puts
-     puts '--' * @board.length
+    0.upto(@board.length - 2) do |row|
+      to_print = []
+      0.upto(@board[row].length - 1) do |i|
+        to_print << if @board[row][i].nil?
+                      row * @board[row].length + (i + 1)
+                    else
+                      @board[row][i]
+                    end
+      end
+      puts to_print.join('|')
+      puts '--' * @board.length
     end
-    0.upto(@board[-1].length-2) do |i| # Print the last row
-      print "#{@board[-1][i]} | "
-     end
+    last_print = []
+    0.upto(@board[-1].length - 1) do |i| # Print the last row
+      if @board[-1][i].nil?
+        last_print << (@board[-1].length - 1) * @board.length + (i + 1)
+      else
+        @board[-1][i]
+      end
+    end
+    puts last_print.join('|')
   end
 
   def play_turn(player)
@@ -54,12 +67,12 @@ class Game
     to_place = gets('Where do you want to play?').chomp.to_i
     place_symbol(to_place, player.symbol)
     to_play = player.next
-    play_turn(to_play) unless win?()
+    play_turn(to_play) unless win?
   end
 end
 
-player1 = Player.new("John", "O")
-player2 = Player.new("Marc", "X")
+player1 = Player.new('John', 'O')
+player2 = Player.new('Marc', 'X')
 game = Game.new(player1, player2)
 
 game.debug
