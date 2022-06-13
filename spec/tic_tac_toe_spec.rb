70fa9fadacce4b449c2a83e1a2e93a35366b9264
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-13 20:49:40 +0200

    Add player and game subjects, test start()

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,3 +1,15 @@
 
 require '../lib/tic_tac_toe.rb'
 
+describe Game do
+  let(:player1) { Player.new("Mark", "O")}
+  let(:player2) { Player.new("John", "X")}
+  subject(:tic_tac_toe) { described_class.new()}
+  describe "#start" do
+    #Does nothing on it's own. Test commands inside it.
+  end
+
+  describe "#play_turn" do
+
+  end
+end
